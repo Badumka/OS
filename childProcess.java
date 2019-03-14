81bbe4eca07a1519b2d22819449109515209601e
Vadim Nikitin <nikitin_vs@phys.vsu.ru>
2019-03-14 16:52:14 +0300

    second task about child and main process

---
+++ childProcess.java
@@ -0,0 +1,23 @@
+class DemoThread implements Runnable {
+    DemoThread() {
+        Thread m = Thread.currentThread(); // Помещается основной поток в переменную m
+        Thread t = new Thread(this, "Demo Thread"); // Создаётся дочерний поток в t
+        t.start(); // Запускается поток t
+        try {
+            Thread.sleep(5000); // Созданный поток переводим в режим ожидания на 5 секунд
+        } catch (InterruptedException e) {
+        }
+    }
+    public void run() { //выполняет простую программму
+        try {
+            for (int i = 5; i > 0; i--) {
+                System.out.println("" + i);
+                Thread.sleep(1000);
+            }
+        } catch (InterruptedException e) {
+        }
+    }
+    public static void main(String args[]) { //вызывает дочерний процесс
+        new DemoThread();
+    }
+}
